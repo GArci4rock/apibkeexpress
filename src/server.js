5b5066c3dfd07791835a6fb2e6a7993fdd66d585
Matheus Garcia <rocha.garcia@aluno.ifsp.edu.br>
2024-08-06 15:56:06 -0300

    add library express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log('ola mundo!')
+console.log('ola Messi!')
+
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json(
+        {
+
+        message:"Olá Messi brasileiro!",
+        success:"sucesso ao executar o Cristiano Ronaldo"
+
+        }
+     )
+})
+
+
+app.listen(3000,() => {
+    console.log('Messi sacudo logado na porta http://localhost:3000')
+})
